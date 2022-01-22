37ba0f876e210ebf702cbba1f865cdbeaff43da6
Vnathn1253 <vishwanath.nijampurkar@fyntune.com>
2022-01-22 12:20:45 +0530

    vendor registration and login done

--- model/Vendor.js
+++ model/Vendor.js
@@ -1,31 +1,40 @@
-const {DataTypes} = require('sequelize');
+const { DataTypes } = require("sequelize");
 
-const {Connection} = require('./Database');
+const { Connection } = require("./Database");
 
-module.exports.Vendor = Connection.define('Vendor', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
-    },
-    name: DataTypes.STRING,
-    mobileNumber: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    shopName: DataTypes.STRING,
-    gstNumber: DataTypes.STRING,
-    foodLicense: DataTypes.STRING,
-    area: DataTypes.STRING,
-    accountStatus: {
-        type: DataTypes.INTEGER,
-        comment: "1:active 2:inactive 3:confirm 4:not confirm"
-    },
-    updateOnColumn: DataTypes.STRING,
-    adminConfirmOn: {
-        type: DataTypes.INTEGER,
-        comment: "1:confirm 0:not confirm"
-    },
-    loginAt: DataTypes.DATE
+module.exports.Vendor = Connection.define("Vendor", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: DataTypes.STRING,
+  mobileNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  shopName: DataTypes.STRING,
+  gstNumber: DataTypes.STRING,
+  foodLicense: DataTypes.STRING,
+  area: DataTypes.STRING,
+  accountStatus: {
+    type: DataTypes.INTEGER,
+    comment: "1:active 2:inactive 3:confirm 4:not confirm",
+  },
+  updateOnColumn: DataTypes.STRING,
+  adminConfirmOn: {
+    type: DataTypes.INTEGER,
+    comment: "1:confirm 0:not confirm",
+  },
+  loginAt: DataTypes.DATE,
 });
--- validator/Schemas.js
+++ validator/Schemas.js
@@ -1,15 +1,25 @@
-const Joi = require('joi');
+const Joi = require("joi");
 const Schemas = {
-    loginValidation: Joi.object(
-        {
-            username: Joi.string().required(),
-            password: Joi.string().required()
-        }),
-    adminRegistration:Joi.object({
-        name: Joi.string().required(),
-        email: Joi.string().email().required(),
-        mobileNumber: Joi.number().integer().min(10).required(),
-        password: Joi.string().required()
-    })
+  loginValidation: Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+  adminRegistration: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    mobileNumber: Joi.number().integer().min(10).required(),
+    password: Joi.string().required(),
+  }),
+  vendorRegistration: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    shopName: Joi.string().required(),
+    mobileNumber: Joi.number().integer().min(10).required(),
+    gstNumber: Joi.string().required(),
+    accountStatus: Joi.number().required(),
+    area: Joi.string().required(),
+    adminConfirmOn: Joi.number().required(),
+  }),
 };
 module.exports = Schemas;
