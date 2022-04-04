0911b3a77308bd928ff098640271f772e54a1d48
vishal <vishalguptaramasare@gmail.com>
2022-04-04 20:57:40 +0530

    changes by vishal

--- app.js
+++ app.js
@@ -17,42 +17,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const diskStorage = multer.diskStorage({
-  destination:(request,file,callback)=>{
-    let destinationPath ="public/";
-    switch (file.fieldname) {
-      case "profileImage":
-        destinationPath += "images/avatar"
-        break;
-      case "idProofImage":
-        destinationPath += "images/avatar"
-        break;
-      case "categoryImage":
-        destinationPath+="images/category"
-        break;
-      case "productImages":
-          destinationPath+="image/products"
+    destination: (request, file, callback) => {
+        let destinationPath = "public/";
+        switch (file.fieldname) {
+            case "profileImage":
+                destinationPath += "images/avatar"
+                break;
+            case "idProofImage":
+                destinationPath += "images/avatar"
+                break;
+            case "categoryImage":
+                destinationPath += "images/category"
+                break;
+            case "productImages":
+                destinationPath += "image/products"
+        }
+        callback(null, destinationPath)
+    },
+    filename: (request, file, callback) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        callback(null, uniqueSuffix + "-" + file.originalname)
     }
-    callback(null,destinationPath)
-  },
-  filename:(request,file,callback)=>{
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    callback(null,uniqueSuffix+"-"+file.originalname)
-  }
 });
-const fileFilter = (request,file,callback)=>{
+const fileFilter = (request, file, callback) => {
 
-  if(file.mimetype==="image/png" ||
-      file.mimetype ==="image/jpg" ||
-      file.mimetype === "image/jpeg"
-  ){
-    callback(null,true)
-  }else{
-    callback(null,false)
-  }
+    if (file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
+    ) {
+        callback(null, true)
+    } else {
+        callback(null, false)
+    }
 }
-app.use(multer({storage:diskStorage,fileFilter:fileFilter}).
-fields([{name:"profileImage",maxCount:1},{name:"categoryImage",maxCount:4},
-  { name: "productImages", maxCount: 4 }]))
+app.use(multer({ storage: diskStorage, fileFilter: fileFilter }).fields([{ name: "profileImage", maxCount: 1 }, { name: "categoryImage", maxCount: 4 },
+    { name: "productImages", maxCount: 4 }
+]))
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
@@ -63,18 +63,18 @@ const webRouter = require("./routes/index");
 
 app.use(webRouter);
 app.use("/admin", adminRouter);
-app.use("/vendor",vendorRouter);
+app.use("/vendor", vendorRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  res.status(err.statusCode || 500).json({message:err.message});
+app.use(function(err, req, res, next) {
+    res.status(err.statusCode || 500).json({ message: err.message });
 });
 
-createDatabase(false,()=>{
-    app.listen(3000, () => console.log("Burans Application Running on Port 3000"));
-});
+// createDatabase(false,()=>{
+app.listen(3000, () => console.log("Burans Application Running on Port 3000"));
+// });
--- routes/index.js
+++ routes/index.js
@@ -8,57 +8,57 @@ const Validator = require('../validator/Validation');
 const Schemas = require('../validator/Schemas');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('login', {title: 'Burans Login'});
+router.get('/', function(req, res, next) {
+    res.render('login', { title: 'Burans Login' });
 });
 
-router.get('/dashboard', function (req, res, next) {
-    res.render('pages/dashboard', {title: 'Burans Login',url:req.url});
+router.get('/dashboard', function(req, res, next) {
+    res.render('pages/dashboard', { title: 'Burans Login', url: req.url });
 });
 
-router.get('/customer', function (req, res, next) {
-    res.render('pages/customer/view_customer', {title: 'View customer',url:req.url});
+router.get('/customer', function(req, res, next) {
+    res.render('pages/customer/view_customer', { title: 'View customer', url: req.url });
 });
 // ---------------------------- Category ---------------------------------
-router.get('/categories', function (req, res, next) {
-    res.render('pages/product/category', {title: 'View product',url:req.url});
+router.get('/categories', function(req, res, next) {
+    res.render('pages/product/category', { title: 'View product', url: req.url });
 });
 
-router.get('/create-categories', function (req, res, next) {
-    res.render('pages/product/CategoryForm', {title: 'View product',url:req.url,categoryId:0});
+router.get('/create-categories', function(req, res, next) {
+    res.render('pages/product/CategoryForm', { title: 'View product', url: req.url, categoryId: 0 });
 });
-router.get('/update-categories', function (req, res, next) {
-    let categoryId= req.params.updateID;
-    res.render('pages/product/CategoryForm', {title: 'View product',url:req.url,categoryId:categoryId});
+router.get('/update-categories', function(req, res, next) {
+    let categoryId = req.params.updateID;
+    res.render('pages/product/CategoryForm', { title: 'View product', url: req.url, categoryId: categoryId });
 });
 
 router.get('/getAllCategories', CategoryController.getAllCategories);
 router.get('/getCategoryById', CategoryController.getCategoryById);
-router.post("/saveCategorySubcategory", Validator(Schemas.categoryValidation),CategoryController.saveCategorySubcategory);
+router.post("/saveCategorySubcategory", Validator(Schemas.categoryValidation), CategoryController.saveCategorySubcategory);
 router.post('/getAllCategoriesOptions', CategoryController.getAllCategoriesOption);
 router.post('/getAllSubcategoriesOption/:categoryId', CategoryController.getAllSubcategoriesOption);
 router.post('/getAllCategoriesTables', CategoryController.getAllCategoriesTables);
-router.put("/updateCategoryStatus",CategoryController.updateCategoryStatus);
-router.delete("/deleteCategory",CategoryController.deleteCategory);
+router.put("/updateCategoryStatus", CategoryController.updateCategoryStatus);
+router.delete("/deleteCategory", CategoryController.deleteCategory);
 
 // --------------------------- product --------------------------------------
 
-router.get('/products', function (req, res, next) {
-    res.render('pages/product/ViewProducts', {title: 'View product',url:req.url});
+router.get('/products', function(req, res, next) {
+    res.render('pages/product/ViewProducts', { title: 'View product', url: req.url });
 });
 
-router.get('/create-product', function (req, res) {
-    res.render('pages/product/ProductForm', {title: 'Create Product',url:req.url,productId:0});
+router.get('/create-product', function(req, res) {
+    res.render('pages/product/ProductForm', { title: 'Create Product', url: req.url, productId: 0 });
 });
-router.get('/update-product/:updateID', function (req, res) {
-    let productId= req.params.updateID;
-    if(!productId){
+router.get('/update-product/:updateID', function(req, res) {
+    let productId = req.params.updateID;
+    if (!productId) {
         let error = new Error('Not Found');
-        error.statusCode=404;
+        error.statusCode = 404;
         throw error
     }
     let segments = req.url.split('/');
-    res.render('pages/product/ProductForm', { title: 'Create Product', url: '/' + segments[1], productId: productId});
+    res.render('pages/product/ProductForm', { title: 'Create Product', url: '/' + segments[1], productId: productId });
 });
 
 
@@ -71,16 +71,19 @@ router.post('/getProductById', ProductController.getProductById);
 
 
 // --------------------------- vendor ------------------------------------------
-router.get('/view-vendor', function (req, res, next) {
-    res.render('pages/vendor/VendorForm', {title: 'View vendor',url:req.url});
+router.get('/view-vendor', function(req, res, next) {
+    res.render('pages/vendor/VendorForm', { title: 'View vendor', url: req.url });
 });
 
-router.get('/create-vendor', function (req, res, next) {
-    res.render('pages/vendor/VendorForm', {title: 'View product',url:req.url});
+router.get('/create-vendor', function(req, res, next) {
+    res.render('pages/vendor/VendorForm', { title: 'View product', url: req.url });
 });
 
-router.get('/create-delivery-boy', function (req, res, next) {
-    res.render('pages/DeliveryBoy/DeliveryBoy', {title: 'View product',url:req.url});
+router.get('/create-delivery-boy', function(req, res, next) {
+    res.render('pages/DeliveryBoy/DeliveryBoy', { title: 'View product', url: req.url });
 });
 
+router.get('/create-offer', function(req, res, next) {
+    res.render('pages/offers/offers', { title: 'View product', url: req.url });
+});
 module.exports = router;
