f97ef0d33732445690abe211ac3645213eb16428
SoftwareClan <53153286+SoftwareClan@users.noreply.github.com>
2022-05-05 22:18:45 +0530

    changes done

--- public/js/osahan.js
+++ public/js/osahan.js
@@ -100,6 +100,7 @@ Version: 1.0
       dots: true
   });
 
+
   // osahan-slider-map
   $('.osahan-slider-map').slick({
       centerMode: true,
@@ -143,6 +144,6 @@ Version: 1.0
   };
 
   // call our plugin
-  var Nav = $main_nav.hcOffcanvasNav(defaultOptions);  
+  var Nav = $main_nav.hcOffcanvasNav(defaultOptions);
 
 })(jQuery); // End of use strict
