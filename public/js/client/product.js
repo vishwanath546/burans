0fd02cc8acd6fb19a5e5e3a1dbc41f278c819295
Vnathn1253 <vishwanath.nijampurkar@fyntune.com>
2022-05-06 18:27:14 +0530

    cart and wishlist done

--- public/js/client/product.js
+++ public/js/client/product.js
@@ -74,12 +74,17 @@ function getSubCategoryProduct(Subcat_id = "") {
       console.log(response.body);
       if (response.status) {
         response.body.forEach((item, value) => {
-          product_list = `<div class="col-6 pr-2" style="margin-bottom: 20px;">
+          product_list += `<div class="col-6 pr-2" style="margin-bottom: 20px;">
           <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm grid-card">
              <div class="list-card-image">
-                <div class="star position-absolute"><span class="badge badge-success"><i class="feather-star"></i> 3.1 (300+)</span></div>
-                <div class="favourite-heart text-danger position-absolute"><a href="#"><i class="feather-heart"></i></a></div>
-                <div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
+                <div class="star position-absolute"><span class="badge badge-success"><i class="feather-star"></i> 3.1 (300+)</span></div>`;
+          if (item.wishlisid != null) {
+            product_list += `<div class="favourite-heart text-danger position-absolute"><a href="#" onclick="addtowishlist(${item.id},${item.subcategoryId})"><i class="feather-heart text-danger" ></i></a></div>`;
+          } else {
+            product_list += `<div class="favourite-heart text-danger position-absolute"><a href="#" onclick="addtowishlist(${item.id},${item.subcategoryId})"><i class="feather-heart text-muted"></i></a></div>`;
+          }
+
+          product_list += `<div class="member-plan position-absolute"><span class="badge badge-dark">Promoted</span></div>
                 <a href="restaurant.html">
                 <img src="${item.image}" class="img-fluid item-img w-100">
                 </a>
@@ -94,6 +99,8 @@ function getSubCategoryProduct(Subcat_id = "") {
                    <span bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"> Rs.${item.price} </span> <strike>${item.salePrice} </strike></p>
                 </div>
                <div class="list-card-badge">
+               <button class="btn btn-danger" onclick="addtocart(${item.id})">Add to cart</button>
+               <button class="btn btn-success" onclick="addtocart(${item.id},1,'buy_now')">Buy Now</button>
                    <span class="badge badge-danger">OFFER</span> <small>65% OSAHAN50</small>
                 </div>
              </div>
@@ -107,3 +114,44 @@ function getSubCategoryProduct(Subcat_id = "") {
       console.log(error);
     });
 }
+
+const addtocart = (product_id, qty = 1, type = "addtocart") => {
+  let data = new FormData();
+  data.set("ProductId", product_id);
+  data.set("qty", qty);
+  app
+    .request("client/add_to_cart", data, "POST")
+    .then((response) => {
+      var product_list = "";
+      if (response.status) {
+        if (type == "buy_now") {
+          window.location.href = "/checkout";
+        } else {
+          app.successToast("Successfully Product added in Cart");
+        }
+        return;
+      }
+      app.errorToast(response.message);
+    })
+    .catch((error) => {
+      app.errorToast("something went wrong");
+    });
+};
+const addtowishlist = (product_id, subcategoryId = "") => {
+  let data = new FormData();
+  data.set("ProductId", product_id);
+  app
+    .request("client/addtowishlist", data, "POST")
+    .then((response) => {
+      var product_list = "";
+      if (response.status) {
+        app.successToast(response.body);
+        getSubCategoryProduct(subcategoryId);
+        return;
+      }
+      app.errorToast(response.message);
+    })
+    .catch((error) => {
+      app.errorToast("something went wrong");
+    });
+};
--- routes/client.js
+++ routes/client.js
@@ -9,5 +9,9 @@ const Homecontroller = require("../controller/client/HomeController");
 Router.get("/getCategory", Homecontroller.getCategory);
 Router.post("/getSubCategory", Homecontroller.getSubCategory);
 Router.post("/getSubCategoryProduct", Homecontroller.getSubCategoryProduct);
+Router.post("/add_to_cart", Homecontroller.add_to_cart);
+Router.post("/getCartList", Homecontroller.getCartList);
+Router.post("/addtowishlist", Homecontroller.addtowishlist);
+Router.post("/getwishList", Homecontroller.getwishList);
 
 module.exports = Router;
